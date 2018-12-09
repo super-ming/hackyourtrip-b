ffebace8cad53700f9ec94256b1895fec1a94166
Vegitarius <gordonv15@gmail.com>
2018-12-08 23:28:20 -0800

    apis working

--- discover-your-trip-frontend/src/components/Feature/Lounges/Lounges.js
+++ discover-your-trip-frontend/src/components/Feature/Lounges/Lounges.js
@@ -5,8 +5,23 @@ export default class Lounges extends Component {
   constructor() {
     super();
     this.state = {
-      city: ''
+      city: 'chicago',
+      lounges: ''
     }
+  }
+
+  componentDidMount() {
+    const corsProxy = "https://cors-anywhere.herokuapp.com/";
+    fetch(corsProxy + `https://apis.discover.com/dci-lounges/v2/lounges?radius=20&city=${this.state.city}&lang=en-us&sortdir=asc `, {
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + this.props.code,
+        'x-dfs-api-plan': 'DCILOUNGES_SANDBOX',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => this.setState({ lounges: data }))
   }
 
   handleCityInput(e) {
@@ -16,6 +31,7 @@ export default class Lounges extends Component {
   }
 
   render() {
+    console.log(this.state.lounges)
     return (
       <div id="lounges">
         {this.state.loungeName}
