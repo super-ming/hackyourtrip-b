435b268465d292a3e3adb68a9a8e151d4062f1ee
Ming Ho <hkboi108@hotmail.com>
2018-12-09 08:15:32 -0800

    Update lounges

--- discover-your-trip-frontend/src/components/Feature/Lounges/Lounges.js
+++ discover-your-trip-frontend/src/components/Feature/Lounges/Lounges.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './Lounges.css';
 import Details from '../Details/details';
-import ImageCarousel from '../../ImageCarousel/ImageCarousel';
 import Feed from '../Feed/feed';
 
 export default class Lounges extends Component {
   constructor() {
     super();
     this.state = {
-      city: 'chicago',
-      lounges: ''
+      city: '',
+      active: 'lounge',
+      lounges: '',
+      refine: null
     }
   }
 
@@ -24,33 +25,65 @@ export default class Lounges extends Component {
       }
     })
     .then(resp => resp.json())
-    .then(data => this.setState({ lounges: data }))
+    .then(data => this.setState({ lounges: [data] })).catch()
+  }
+
+  getLounges() {
+    const corsProxy = "https://cors-anywhere.herokuapp.com/";
+    fetch(corsProxy + `https://apis.discover.com/dci-lounges/v2/lounges?radius=20&city=${this.state.city}&lang=en-us&sortdir=asc `, {
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + this.props.code,
+        'x-dfs-api-plan': 'DCILOUNGES_SANDBOX',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => this.setState({ lounges: [data] }))
   }
 
   handleCityInput(e) {
     this.setState({
-      country: e.target.value
+      city: e.target.value
     })
   }
 
+  showList(){
+    console.log(this.state.lounges);
+    if(this.state.lounges != null){
+      return <div>{this.state.lounges}</div>
+    /*  return(
+        (this.state.lounges[0].result.forEach((lounge, index) => (
+          <div className="lounge-list">{}</div>
+        )))
+      )*/
+    } else {
+      return
+    }
+  }
+
   render() {
-    let lounges = ['London', 'Bangkok', 'Miami', 'Singapore', 'New York']
+
+    console.log(this.state.city);
+    console.log(this.state.lounges);
+    console.log(this.state.lounges)
     return (
       <div id="lounges">
         {this.state.loungeName}
         <div className="cityChoose">
           Find Lounges:
           <input onChange={(e) => this.handleCityInput(e)} placeholder={"Enter a city..."}></input>
+          <button onClick={()=>this.getLounges()}>Refine</button>
         </div>
-        <div className="window d-flex">
-          <ImageCarousel className="img-carousel"/>
-          <Details name={this.state.route}/>
+        <div className="window">
+          { this.showList()
+          }
+
+          <Details active={this.state.active} lounges={this.state.lounges}>
+          </Details>
           <Feed></Feed>
           </div>
-        </div>
->>>>>>> front
       </div>
-
     )
   }
 }
