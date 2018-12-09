a8cc6e818c6b56d538da2b8d41b0902213d4aaad
Ming Ho <hkboi108@hotmail.com>
2018-12-08 21:31:03 -0800

    Remove redundant files

--- discover-your-trip-frontend/src/components/Feature/Lounges/Lounges.js
+++ discover-your-trip-frontend/src/components/Feature/Lounges/Lounges.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import './Lounges.css';
-||||||| initial frontend
-=======
 import './Lounges.css';
 import Details from '../Details/details';
 import ImageCarousel from '../../ImageCarousel/ImageCarousel';
->>>>>>> front
 
 export default class Lounges extends Component {
   constructor() {
@@ -24,17 +19,6 @@ export default class Lounges extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <div id="lounges">
-        {this.state.loungeName}
-        <div className="cityChoose">
-          City: 
-          <input onChange={(e) => this.handleCityInput(e)}></input>
-        </div>
-||||||| initial frontend
-      <div>
-        Information from api about lounges
-=======
       <div id="lounges">
         {this.state.loungeName}
         <div className="cityChoose">
@@ -45,9 +29,7 @@ export default class Lounges extends Component {
           <ImageCarousel />
           <Details name={this.state.route}/>
         </div>
->>>>>>> front
       </div>
-
     )
   }
 }
